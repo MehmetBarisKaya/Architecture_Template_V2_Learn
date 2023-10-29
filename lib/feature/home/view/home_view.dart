c1e201402dcd11b27fce10b8662a680ca3253485
Baris Kaya <baris.kaya99@hotmail.com>
2023-10-30 01:32:51 +0300

    Creating folder structure and  module creation

---
+++ lib/feature/home/view/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:architecture_template_v2/feature/home/view/mixin/home_view_mixin.dart';
+import 'package:flutter/material.dart';
+
+part 'widget/home_app_bar.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> with HomeViewMixin {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: _HomeAppBar(),
+    );
+  }
+}
---
+++ lib/feature/home/view/mixin/home_view_mixin.dart
@@ -0,0 +1,4 @@
+import 'package:architecture_template_v2/feature/home/view/home_view.dart';
+import 'package:flutter/material.dart';
+
+mixin HomeViewMixin on State<HomeView> {}
