dce129160a59852cb7a4c919a2102939c9263308
Baris Kaya <68561489+MehmetBarisKaya@users.noreply.github.com>
2023-10-31 14:19:24 +0300

    Revert "Auto route added"
    This reverts commit c4aaaa09558b91f713f3251cc6dea86d983ab6bd.

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -2,16 +2,12 @@ import 'package:architecture_template_v2/feature/home/view/mixin/home_view_mixin
 import 'package:architecture_template_v2/product/init/config/app_environment.dart';
 import 'package:architecture_template_v2/product/init/language/locale_keys.g.dart';
 import 'package:architecture_template_v2/product/init/product_localization.dart';
-import 'package:architecture_template_v2/product/navigation/deeplink/app_router.dart';
 import 'package:architecture_template_v2/product/utility/constant/enums/locales.dart';
-import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:gen/gen.dart';
 
 part 'widget/home_app_bar.dart';
-
-@RoutePage()
 
 /// App Home View
 class HomeView extends StatefulWidget {
@@ -48,12 +44,7 @@ class _HomeViewState extends State<HomeView> with HomeViewMixin {
               child: const Text('English'),
             ),
             ElevatedButton(
-              onPressed: () async {
-                final response =
-                    await context.router.push<bool>(HomeDetailRoute(id: '1'));
-                // ignore: avoid_print
-                print(response);
-              },
+              onPressed: () {},
               child: Text(AppEnvironmentItems.baseUrl.value),
             ),
           ],
